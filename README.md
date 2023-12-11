0b1965e0f98fdb1e99ee334550d4b16290515acb
Derick Hermanson <derick.hermanson@gmail.com>
2023-12-11 13:49:00 -0600

    play around with swift

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Swift Playground
 This is just a play for me to learn Swift.
+
+
+## Creating a command-line tool
+```sh
+swift package init --name MyCLI --type executable
+```
---
+++ Sources/main.swift
@@ -0,0 +1,24 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import Figlet
+
+class Person {
+
+    private let name: String
+
+    init(name: String) {
+        self.name = name
+    }
+    
+}
+
+Figlet.say("hello, swift!")
+
+let derick = Person(name: "derick")
+
+print("Hello, world!")
+
+let x = 3
+
+let y = "foobar"
