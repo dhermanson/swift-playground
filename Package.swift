32f5a9c5b28b44a627c6467405d64a7e88ed5f9f
Derick Hermanson <derick.hermanson@gmail.com>
2023-12-15 09:11:58 -0600

    WIP on using postgresql

--- Package.swift
+++ Package.swift
@@ -1,11 +1,16 @@
 // swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
+
 // see https://forums.swift.org/t/package-dependency-in-own-package/64587/2
+// see: http://www.openradar.appspot.com/FB7654249
 
 import PackageDescription
 
 let package = Package(
   name: "MyLibrary",
+  platforms: [
+    .macOS(.v10_15)
+  ],
   products: [
     // Products define the executables and libraries a package produces, making them visible to other packages.
     .library(
@@ -14,6 +19,8 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/apple/example-package-figlet", branch: "main"),
+    // .package(url: "https://github.com/vapor/postgres-nio.git", from: "1.14.0"),
+    .package(url: "https://github.com/vapor/postgres-kit.git", from: "2.12.2"),
   ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -21,7 +28,8 @@ let package = Package(
     .target(
       name: "MyLibrary",
       dependencies: [
-        .product(name: "Figlet", package: "example-package-figlet")
+        .product(name: "Figlet", package: "example-package-figlet"),
+        .product(name: "PostgresKit", package: "postgres-kit"),
       ]
     ),
     .testTarget(
--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,8 +1,31 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 import Figlet
+import PostgresNIO
+import Logging
 
 public enum MyLibrary {
+
+    public func start() async throws {
+
+        let config = PostgresConnection.Configuration(
+          host: "localhost",
+          port: 5432,
+          username: "user",
+          password: "secret",
+          database: "defaultdb",
+          tls: .disable
+        )
+
+        let logger = Logger(label: "postgres-logger")
+
+        let connection = try await PostgresConnection.connect(
+          configuration: config, id: 1, logger: logger
+        )
+
+        try await connection.close()
+        
+    }
 
     public class Person {
 
