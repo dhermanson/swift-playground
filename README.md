5b7eb70f5828e0e74e78e33e1a512b335f1af58a
Derick Hermanson <derick.hermanson@gmail.com>
2023-12-15 15:42:53 -0600

    successfully query from postegresql

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ This is just a place for me to learn Swift.
 ```sh
 swift package init --name MyCLI --type executable
 ```
+
+
+## Links
+- observe how [SwiftNIO EventLoopGroup](https://github.com/apple/swift-nio/blob/main/Sources/NIOChatServer/main.swift#L119) works
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,20 +1,43 @@
 import PostgresKit
 
-let config = PostgresConnection.Configuration(
-  host: "localhost",
-  port: 5432,
-  username: "user",
-  password: "secret",
-  database: "defaultdb",
-  tls: .disable
-)
 
 let logger = Logger(label: "postgres-logger")
 
-let connection = try await PostgresConnection.connect(
-  configuration: config, id: 1, logger: logger
-)
+let eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: System.coreCount)
 
-try await connection.close()
+defer {try! eventLoopGroup.syncShutdownGracefully()}
+
+let configuration = PostgresConfiguration(
+  hostname: "localhost",
+  username: "user",
+  password: "secret",
+  database: "defaultdb"
+)
+let db = PostgresConnectionSource(configuration: configuration)
+let pool = EventLoopGroupConnectionPool(
+  source: db,
+  on: eventLoopGroup)
+
+defer {pool.shutdown()}
+
+let x = await try pool.withConnection { conn in
+                conn.query("SELECT 1;")
+}.get()
+
+
+print("result: \(x)")
+
+// let res = pools.withConnection { conn -> in
+//                        return "test"
+// }
+
+// pools.withConnection(
+// pools.withConnection { conn in
+// }
+// // pools.withConnection { conn
+// // }
+
+
+// try await connection.close()
 
 print("done!!\n")
